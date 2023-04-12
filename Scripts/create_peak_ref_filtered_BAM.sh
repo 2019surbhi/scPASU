d69666600d94a815ace1128e93c4967c6517a1a3
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-04-12 00:55:50 -0400

    Update create_peak_ref_filtered_BAM.sh

--- Scripts/create_peak_ref_filtered_BAM.sh
+++ Scripts/create_peak_ref_filtered_BAM.sh
@@ -20,3 +20,11 @@ out='/home/sonas/beegfs/APA/scPASU/output/7_UCSC_uploads/peak_filtered_bam/'
 
 bedtools intersect -abam ${BAM} -b ${peak_ref} -wa > ${out}u10_uro_peak_filtered.bam
 
+bam_dir='/home/sonas/beegfs/APA/scPASU/output/3_PeakCounts/3b_SplitBAM_per_clus/'
+bam_pre='2021_04_01_ureter10_uro_PC50_res0.2_cluster'
+peak_ref='/home/sonas/beegfs/APA/scPASU/output/2_PeakRef/2e_TSS_filtered/u10_uro_l300iA10mm3_e100_updated_tss_filt_final.bed'
+out='/home/sonas/beegfs/APA/scPASU/output/7_UCSC_uploads/peak_filtered_bam/'
+
+clus=$SLURM_ARRAY_TASK_ID
+
+bedtools intersect -abam ${bam_dir}${bam_pre}${clus}.bam -b ${peak_ref} -wa > ${out}cluster${clus}_peak_filtered.bam
