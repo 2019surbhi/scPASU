be974507f0a042bf86a07e8e2cfc5c23bd7c4b17
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-06 20:41:18 -0500

    Create 2a_peak_calling.sh

---
+++ Scripts/2_peak_ref/2a_peak_calling.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sonas@ccf.org
+#SBATCH --job-name=plus_MACS2_peak_calling
+#SBATCH --cpus-per-task=30
+#SBATCH --mem-per-cpu=3900
+
+#SBATCH -o macs2_u10_uro_p.out
+#SBATCH -e macs2_u10_uro_p.err
+
+#module load R/4.1.0
+
+module load samtools
+
+
+bam_dir='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1e_merged_bam/'
+out_dir='/home/sonas/beegfs/APA/scPASU/output/2_PeakRef/2a_peaks/'
+
+strand='plus'
+strand='minus'
+
+bam_prefix=dedup_u10_uro_clean_${strand}
+
+# Run MACS2
+
+cd ${bam_dir}
+
+macs2 callpeak \
+	-t ${bam_dir}${bam_prefix}.bam \
+	--keep-dup all \
+	--gsize hs \
+	--format BAM \
+	--nomodel \
+        --extsize 200 \
+        --shift -100 \
+	--outdir ${out_dir} \
+	--name ${bam_prefix} \
+	-B \
+	--verbose 2 
