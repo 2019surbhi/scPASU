b988b33133f7e8efe6ffca896b8ded5c829f3d7d
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-06 19:43:30 -0500

    Update and rename 1_preprocess_bam.sh to 1b_preprocess_bam.sh

--- Scripts/1_data_preprocessing/1b_preprocess_bam.sh
+++ Scripts/1_data_preprocessing/1b_preprocess_bam.sh
@@ -7,8 +7,8 @@
 #SBATCH -n 20
 #SBATCH --mem-per-cpu=3900
 
-#SBATCH -o preprocess_bam_ureter%J.out
-#SBATCH -e preprocess_bam_ureter%J.err
+#SBATCH -o preprocess_bam_%J.out
+#SBATCH -e preprocess_bam_%J.err
 
 export LD_LIBRARY_PATH=/cm/local/apps/gcc/8.2.0/lib64/
 module load gcc/8.2.0
@@ -16,38 +16,44 @@ module load samtools/1.9
 module load subset-bam/1.1.0
 module load python/3.8.6 #To use umi_tools
 
-barcodes='/home/sonas/beegfs/APA/scAPA/ureter10/uro_barcodes/'
-bam_dir='/home/sonas/beegfs/APA/scAPA/ureter10/ureter_bam/'
-subset_dir='/home/sonas/beegfs/APA/scAPA/ureter10/u10_uro_bam/'
-dedup_dir='/home/sonas/beegfs/APA/scAPA/ureter10/dedup_u10_uro_bam/'
-filter_dir='/home/sonas/beegfs/APA/scAPA/ureter10/dedup_u10_uro_filterd_bam/'
+# Input dir #
+input_dir='/home/sonas/beegfs/APA/scPASU/input/'
+barcodes=${input_dir}'/barcode/'
+bam_dir=${input_dir}'/bam/'
 
-export samples_file='/home/sonas/beegfs/APA/scAPA/ureter10/ureter.txt'
+# Output directories #
+outdir='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/'
+subset_dir=${outdir}'/1a_subset_bam/'
+dedup_dir=${outdir}'/1b_dedup_bam/'
+clean_bam_dir=${outdir}'/1c_clean_bam/'
+filter_dir=${outdir}'/1d_filtered_bam/'
 
-
-
+export samples_file=${input_dir}'ureter_samples.txt'
 sample=$(cat ${samples_file} | nl -w1 -s ' ' | grep "^$SLURM_ARRAY_TASK_ID " | cut -f2 -d ' ')
 
 ## 1. Subset ##
 
 subset-bam -b ${bam_dir}${sample}.bam -c ${barcodes}${sample}_uro_barcodes.tsv -o ${subset_dir}${sample}_uro.bam --log-level debug
-
 samtools index ${subset_dir}${sample}_uro.bam
 
 ## 2. Dedup ##
 
 umi_tools dedup -I ${subset_dir}${sample}_uro.bam -S ${dedup_dir}dedup_${sample}_uro.bam --method=unique --extract-umi-method=tag --umi-tag=UB --cell-tag=CB
-
 samtools index ${dedup_dir}dedup_${sample}_uro.bam
 
-## 3. Filter genomicA reads ##
+## 3. Cleanup BAM - only uniquely mapped reads retained ##
+
+#samtools view -b -q 1 ${dedup_dir}dedup_${sample}_uro.bam > ${clean_bam_dir}dedup_uniq_${sample}_uro.bam
+#samtools index ${clean_bam_dir}dedup_uniq_${sample}_uro.bam
+
+## 4. Filter genomicA reads ##
 
 WORKING_DIR='/home/sonas/beegfs/tools/polyAfilter/'
 
 FASTA="/home/sonas/beegfs/ref/refdata-gex-GRCh38-2020-A/fasta/genome.fa"
 GTF="/home/sonas/beegfs/ref/refdata-gex-GRCh38-2020-A/genes/genes.gtf"
 
-BAM=${dedup_dir}dedup_${sample}_uro.bam
+BAM=${clean_bam_dir}dedup_uniq_${sample}_uro.bam
 
 DB=${filter_dir}"gtfdb.db"
 TRANS=${filter_dir}${sample}"_trans.pkl" # Note this is a cache file so define with sample name if running in parallel
