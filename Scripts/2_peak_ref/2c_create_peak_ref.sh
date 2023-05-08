c3bdd583c7e76ed307d6c0810450266a05485302
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-08 14:28:54 -0500

    Update 2c_create_peak_ref.sh

--- Scripts/2_peak_ref/2c_create_peak_ref.sh
+++ Scripts/2_peak_ref/2c_create_peak_ref.sh
@@ -45,15 +45,14 @@ cd ${out_dir}
 outdir=${out_dir}${strand}/
 mkdir ${outdir}
 
-peaks_dir=${outdir}peaks_dir/
-polya_dir=${outdir}polya_dir/
-int_dir=${outdir}int_dir/
-pr_dir=${outdir}pr_dir/
-pr_table_dir=${outdir}pr_table_dir/
-peaks_count_dir=${outdir}peaks_count_dir/
-polya_count_dir=${outdir}polya_count_dir/
-ref_table=${outdir}ref_table/
-filtered_ref_table=${outdir}filtered_ref_table/
+peaks_per_chr=${outdir}0_peaks_per_chr/
+polya_per_chr=${outdir}0_polya_per_chr/
+int_dir=${outdir}1_int_dir/
+peaks_count_dir=${outdir}2a_peaks_count_dir/
+polya_count_dir=${outdir}2b_polya_count_dir/
+peak_ref_dir=${outdir}3a_peak_ref_table/
+pr_dir=${outdir}3b_pr_dir/
+final_peak_ref=${outdir}4_final_peak_ref/
 
 ### 0. Prepare files ###
 
@@ -61,13 +60,14 @@ filtered_ref_table=${outdir}filtered_ref_table/
 
 cd ${outdir}
 
-mkdir ${peaks_dir}
-mkdir ${polya_dir}
+mkdir ${peaks_per_chr}
+mkdir ${polya_per_chr}
 mkdir ${int_dir}
-mkdir ${pr_dir}
-mkdir ${pr_table_dir}
 mkdir ${peaks_count_dir}
 mkdir ${polya_count_dir}
+mkdir ${pr_dir}
+mkdir ${peak_ref_table}
+
 mkdir ${ref_table}
 mkdir ${filtered_ref_table}
  
@@ -90,36 +90,35 @@ echo creating peak and polya files by chr
 
 if [ ${strand} == 'plus' ]
 then
-   cat ${peaks_file} | grep -w ${chr} | awk 'OFS="\t" {print $1,$2,$3=$3+ '$extn',$4,$5="+"}' > ${peaks_dir}peaks_${chr}_${strand}.bed
+   cat ${peaks_file} | grep -w ${chr} | awk 'OFS="\t" {print $1,$2,$3=$3+ '$extn',$4,$5="+"}' > ${peaks_per_chr}peaks_${chr}_${strand}.bed
 else
-   cat ${peaks_file} | grep -w ${chr} | awk 'OFS="\t" {print $1,$2=$2- '$extn',$3,$4,$5="-"}' > ${peaks_dir}peaks_${chr}_${strand}.bed
+   cat ${peaks_file} | grep -w ${chr} | awk 'OFS="\t" {print $1,$2=$2- '$extn',$3,$4,$5="-"}' > ${peaks_per_chr}peaks_${chr}_${strand}.bed
 fi
  
 # subset poly table too
 
-cat ${polya_file} | grep -w ${chr} | awk 'OFS="\t" {print $1,$2,$3,$4,$5,$6}' > ${polya_dir}polya_${chr}_${strand}.bed
+cat ${polya_file} | grep -w ${chr} | awk 'OFS="\t" {print $1,$2,$3,$4,$5,$6}' > ${polya_per_chr}polya_${chr}_${strand}.bed
 
 
 # (e) sort 
 
 echo sort 
 
-cat  ${peaks_dir}peaks_${chr}_${strand}.bed | sort -k2,2n -k3,3n >  ${peaks_dir}peaks_${chr}_sorted_${strand}.bed
-cat  ${polya_dir}polya_${chr}_${strand}.bed | sort -k2,2n -k3,3n >  ${polya_dir}polya_${chr}_sorted_${strand}.bed
+cat  ${peaks_per_chr}peaks_${chr}_${strand}.bed | sort -k2,2n -k3,3n >  ${peaks_per_chr}peaks_${chr}_sorted_${strand}.bed
+cat  ${polya_per_chr}polya_${chr}_${strand}.bed | sort -k2,2n -k3,3n >  ${polya_per_chr}polya_${chr}_sorted_${strand}.bed
 
 ### 1a. Run intersect ###
 
 echo intersect
 
-bedtools intersect -a ${peaks_dir}peaks_${chr}_sorted_${strand}.bed -b ${polya_dir}polya_${chr}_sorted_${strand}.bed -wa -wb -loj -s > ${int_dir}peaks_int_polya_${chr}_${strand}.bed
+bedtools intersect -a ${peaks_per_chr}peaks_${chr}_sorted_${strand}.bed -b ${polya_per_chr}polya_${chr}_sorted_${strand}.bed -wa -wb -loj -s > ${int_dir}peaks_int_polya_${chr}_${strand}.bed
 
 
 ### 1b. Sort intersect table ###
 cat ${int_dir}peaks_int_polya_${chr}_${strand}.bed | sort -k6,6n  > ${int_dir}peaks_int_polya_${chr}_sorted_${strand}.bed
 
-### 2. Process table ###
 
-### 2a. Tag non-poly peaks
+### 1c. Tag non-poly peaks
 
 awk  'FS=OFS="\t" {if($6==".") {$12="non_polya"} else {$12="polya"}; {print $0}}' ${int_dir}peaks_int_polya_${chr}_sorted_${strand}.bed > ${int_dir}peaks_int_polya_${chr}_tagged_${strand}.bed
 
@@ -128,14 +127,14 @@ awk  'FS=OFS="\t" {if($6==".") {$12="non_polya"} else {$12="polya"}; {print $0}}
 #fname=peaks_int_polya_${chr}_tagged_filtered_${strand}.bed
 #Rscript ${script_dir}cleanup_peak_ref.R ${file} ${int_dir} 
 
-### 2b. Sort by polya column
+### 1d. Sort by polya column
 
 cat ${int_dir}peaks_int_polya_${chr}_tagged_filtered_${strand}.bed | sort -k12,12 > ${int_dir}peaks_int_polya_${chr}_tagged_sorted_${strand}.bed
 
-### 3a. Create peaks count
+### 2a. Create peaks count
 cat ${int_dir}peaks_int_polya_${chr}_tagged_filtered_${strand}.bed | grep -w polya | awk 'FS=OFS="\t" {print $4}' | sort | uniq -c | sort -k1,1n > ${peaks_count_dir}peaks_${chr}_count_sorted_${strand}.txt
 
-### 3.b. Create polya read count per peak
+### 2b. Create polya read count per peak
 cd ${int_dir}
 file=${int_dir}peaks_int_polya_${chr}_tagged_filtered_${strand}.bed
 peaks_file=${peaks_count_dir}peaks_${chr}_count_sorted_${strand}.txt
@@ -150,12 +149,12 @@ done
 cd ${polya_count_dir}
 cat polya_${chr}_count_${strand}.txt | sort -k1,1n > polya_${chr}_count_sorted_${strand}.txt 
 
-### 3c. Create PR table ###
+### 3a. Create peak ref table ###
 
-cat ${int_dir}peaks_int_polya_${chr}_tagged_sorted_${strand}.bed | sort -u -k4,4  > ${pr_table_dir}pr_table_${chr}_${strand}.bed
+cat ${int_dir}peaks_int_polya_${chr}_tagged_sorted_${strand}.bed | sort -u -k4,4  > ${peak_ref_table}peak_ref_${chr}_${strand}.bed
 
 
-### 4. Get inferred PR ###
+### 3b. Create inferred PR table ### (contains 3 columns - peakID, inf_pr_start and inf_pr_end)
 
 f=$(awk '{print $2}' ${peaks_count_dir}peaks_${chr}_count_sorted_${strand}.txt)
 
@@ -163,17 +162,17 @@ for peaks in ${f[*]};do
 cat ${int_dir}peaks_int_polya_${chr}_sorted_${strand}.bed | grep -w ${peaks}  | awk '{if(min==""){min=max=$7}; if($8>max) {max=$8}; if($7< min) {min=$7};} END {print $4"\t"min"\t"max}' >> ${pr_dir}pr_${chr}_${strand}.bed
 done
 
-# Sort
+# 3c. Sort
 
 cat ${pr_dir}pr_${chr}_${strand}.bed | sort -k2,2n -k3,3n > ${pr_dir}pr_${chr}_sorted_${strand}.bed
 
-### 5. Final PR table ###
+### 3d. Add inferred PR, peak & PR width and polyA count  ###
 
-Rscript ${script_dir}2c_create_peak_ref.R ${outdir} ${strand} ${chr}
+Rscript ${script_dir}2c_update_peak_ref.R ${outdir} ${strand} ${chr}
 
 # Filter peaks by min polya reads
 
-bed_file=${ref_table}ref_table_updated_${chr}_${strand}.bed
+#bed_file=${ref_table}ref_table_updated_${chr}_${strand}.bed
 
-Rscript ${script_dir}2c_filter_peak_ref.R ${bed_file} ${filtered_ref_table} ${min_polya}
+#Rscript ${script_dir}2c_filter_peak_ref.R ${bed_file} ${filtered_ref_table} ${min_polya}
 
