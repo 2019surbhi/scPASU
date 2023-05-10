4f3076f5b4684e67227d1c9d8ec81301f43cd5cb
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-10 14:21:55 -0500

    Update 3b_update_peak_ref.sh

--- Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
+++ Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
@@ -3,11 +3,11 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=sonas@ccf.org
 #SBATCH --job-name=final_peak_ref
-#SBATCH -n 20
+#SBATCH -n 20 -p xtreme
 #SBATCH --mem-per-cpu=3900
 
-#SBATCH -o peak_ref%J.out
-#SBATCH -e peak_ref_%J.err
+#SBATCH -o peak_cov%J.out
+#SBATCH -e peak_cov%J.err
 
 
 ### Load modules ###
@@ -16,59 +16,19 @@ module load R/4.1.0
 
 ### Initialize variables ###
 
-extn=25
-#extn=50
-#extn=75
-#extn=100
-#extn=125
-#extn=149
+strand='minus'
+strand='plus'
 
+cores=30
+bam_dir='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1e_merged_bam/'
+bam=dedup_u10_uro_clean_filtered2_${strand}.bam
 
-outdir='/home/sonas/beegfs/APA/scPASU/output/2_PeakRef/2d_TU_annotated/'
-fprefix='u10_uro_l300iA10mm3_e'${extn}
+ref='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3a_assign_TU/u10_uro_peak_universe_updated.saf'
+outdir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage'
+fprefix='u10_uro'
 
 script_dir='/home/sonas/beegfs/APA/scPASU/scripts/'
-${script_dir}update_peak_ref.R ${peak_ref_dir} ${outdir} ${fprefix}
+Rscript ${script_dir}feature_counts.R ${bam} ${ref} ${outdir} ${fprefix} ${cores} no
 
 
 
-### Archive ###
-
-script_dir='/home/sonas/beegfs/my_scripts/pipeline/my_scAPA/peak_ref/'
-bash ${script_dir}create_final_peak_ref.sh ${peak_ref_dir} ${outdir} ${fprefix}
-
-
-#peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/u10_uro_unfiltered_3polya/'
-#fprefix='u10_uro_unfiltered_3polya'
-
-
-#peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/output_3polyA/'
-#_A10mm0_3polya'
-
-#peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/l300i_A6mm0_3polyA/'
-#fprefix='u10_uro_final_peak_ref_l300i_A6mm0_3polya'
-
-#peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/l300i_A10mm3_3polyA/'
-#fprefix='u10_uro_final_peak_ref_l300i_A10mm3_3polya'
-
-# Run these
-peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/l250i_A10mm3_3polyA/'
-fprefix='u10_uro_final_peak_ref_l250i_A10mm3_3polya'
-
-peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/l200i_A10mm3_3polyA/'
-fprefix='u10_uro_final_peak_ref_l200i_A10mm3_3polya'
-
-#peak_ref_dir='/home/sonas/beegfs/APA/scAPA/ureter10/assign_tu/l300i_A10mm2_3polyA/'
-#fprefix='u10_uro_final_peak_ref_l300i_A10mm2_3polya'
-
-# Peak ref after TU assignment
-script_dir='/home/sonas/beegfs/my_scripts/pipeline/my_scAPA/peak_ref/'
-bash ${script_dir}create_final_peak_ref.sh ${peak_ref_dir} ${outdir} ${fprefix}
-
-# TSS filtered ref - add code for calling respective script here
-
-
-# Create SAF format ref too
-peak_ref_file=${outdir}${fprefix}_updated_tss_filt.txt
-
-Rscript ${script_dir}create_saf.R ${outdir} ${peak_ref_file} ${fprefix}
