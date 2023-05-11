3e93dad0a1b64c2afa68876e461eaba53e494481
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-11 02:01:55 -0500

    Update 3b_filter_peak_count.R

--- Scripts/3_peak_ref_cleanup/3b_filter_peak_count.R
+++ Scripts/3_peak_ref_cleanup/3b_filter_peak_count.R
@@ -14,11 +14,6 @@ fprefix=args[3]
 min_cov=as.numeric(args[4])
 is_minus=as.logical(args[5]) # TRUE or FALSE
 
-# HPC paths
-pcov_file<-'/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage2/u10_uro_minus_peak_count_updated.rds'
-out<-'/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3c_updated_peaks/'
-fprefix<-'u10_uro_minus'
-
 pcov_mat<-readRDS(pcov_file)
 rem<-which(pcov_mat$pcov_pct<min_cov)
 pcov_mat2<-pcov_mat[-rem,]
