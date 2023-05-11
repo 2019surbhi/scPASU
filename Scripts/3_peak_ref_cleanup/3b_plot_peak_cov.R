97c912acc66488f79e4d3ee859019ef9d1734476
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-10 22:06:33 -0500

    Update 3b_plot_peak_cov.R

--- Scripts/3_peak_ref_cleanup/3b_plot_peak_cov.R
+++ Scripts/3_peak_ref_cleanup/3b_plot_peak_cov.R
@@ -3,8 +3,29 @@
 library(dplyr)
 library(data.table)
 
+source('/home/sonas/beegfs/APA/scPASU/scripts/scPASU_functions.R')
+
 args <- commandArgs(trailing = TRUE)
 
-ref_file=args[1]
-counts_file=args[2]
+pcov_file=args[1]
+out=args[2]
+fprefix=args[3]
+w=args[4]
+h=args[5]
+x=args[6]
+y=args[7]
+
+#HPC paths
+# pcov_file='u10_uro_plus_peak_by_cell_count_updated.rds'
+# out='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage2/'
+# fprefix='u10_uro_plus'
+
+pcov_mat<-readRDS(pcov_file)
+
+dat<-pcov_mat$pcov_pct
+
+options(bitmapType='cairo')
+pdf(paste0(out,fprefix,'_peak_cov_pct.pdf'),width=w, height=h)
+get_histogram(dat=dat,binw=1,col='royal blue',fill='salmon', x_lim=c(0,100),y_lim=c(0,5000),title='Peak read count percentage',x_lab='peak_read_pct')
+dev.off()
 
