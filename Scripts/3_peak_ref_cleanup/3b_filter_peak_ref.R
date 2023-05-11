259deb91c456ea52d82943a2b8aed5f5de7a8e66
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-11 01:28:45 -0500

    Update 3b_filter_peak_ref.R

--- Scripts/3_peak_ref_cleanup/3b_filter_peak_ref.R
+++ Scripts/3_peak_ref_cleanup/3b_filter_peak_ref.R
@@ -3,6 +3,8 @@
 library(dplyr)
 library(data.table)
 
+source('/home/sonas/beegfs/APA/scPASU/scripts/scPASU_functions.R')
+
 args <- commandArgs(trailing = TRUE)
 
 
@@ -10,6 +12,7 @@ pcov_file=args[1]
 out=args[2]
 fprefix=args[3]
 min_cov=as.numeric(args[4])
+is_minus=as.logical(args[5]) # TRUE or FALSE
 
 # HPC paths
 pcov_file<-'/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage2/u10_uro_minus_peak_count_updated.rds'
@@ -19,3 +22,15 @@ fprefix<-'u10_uro_minus'
 pcov_mat<-readRDS(pcov_file)
 rem<-which(pcov_mat$pcov_pct<min_cov)
 pcov_mat2<-pcov_mat[-rem,]
+
+# Subset TUs that have now single peaks
+cnt<-pcov_mat2 %>% group_by(tu) %>% tally()
+cnt<-cnt %>% arrange(.,n)
+r<-which(cnt$n==1)
+p0_mat<-pcov_mat2[pcov_mat2$tu %in% cnt$tu[r],]
+pn_mat<-pcov_mat2[!(pcov_mat2$tu %in% cnt$tu[r]),]
+
+pn_mat2<-create_final_annotation_col(pn_mat,is_minus=is_minus)
+pcov_mat3<-rbind(p0_mat,pn_mat)
+saveRDS(pcov_mat3,paste0(out,fprefix,'_filtered_peak_count.rds))
+
