99ed5d9a14188a5b987e482ff32706a4128c931b
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-10 23:50:00 -0500

    Update feature_counts.R

--- Scripts/feature_counts.R
+++ Scripts/feature_counts.R
@@ -19,10 +19,10 @@ isGTF<-args[6] # Enter yes or no
 if(isGTF=='yes')
 {
   counts<-featureCounts(bam_file, annot.ext=ref, isGTFAnnotationFile=TRUE, GTF.attrType='gene_name', strandSpecific=1, largestOverlap = T, nthreads = cores)
-  fname<-paste0(out,prefix,'_gene_by_cell_count.rds')
+  fname<-paste0(out,prefix,'_gene_count.rds')
 }else{
   counts<-featureCounts(bam_file, annot.ext = ref, isGTFAnnotationFile = FALSE,strandSpecific=1, largestOverlap = T, nthreads = cores)
-  fname<-paste0(out,prefix,'_peak_by_cell_count.rds')
+  fname<-paste0(out,prefix,'_peak_count.rds')
 }
 
 saveRDS(counts, fname)
