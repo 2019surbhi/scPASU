8fb2e9641427cd5733c3e05bb10f90559f57d6dc
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-14 14:54:15 -0500

    Update 3b_update_peak_ref.sh

--- Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
+++ Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
@@ -21,14 +21,13 @@ is_minus='FALSE'
 
 cores=30
 bam_dir='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1e_merged_bam/'
-#bam=${bam_dir}dedup_u10_uro_clean_filtered_${strand}.bam
-bam=${bam_dir}dedup_u10_uro_clean_filtered2_2_${strand}.bam
+bam=${bam_dir}dedup_u10_uro_clean_filtered2_2_merged.bam
 
 ref_dir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3a_assign_TU/'
 ref_saf=${ref_dir}u10_uro_filtered_100_peak_universe_updated.saf
 
 outdir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage/'
-fprefix=u10_uro_filtered_100_${strand}
+fprefix=u10_uro_filtered_100
 
 script_dir='/home/sonas/beegfs/APA/scPASU/scripts/'
 
@@ -49,8 +48,8 @@ Rscript ${script_dir}3b_peaks_coverage.R ${ref} ${counts_file} ${outdir}
 # 3.  Plot peak coverage percent
 
 echo peak coverage plot 
-pcov_file=${outdir}u10_uro_filtered_100_${strand}_peak_count_updated.rds
-fprefix=u10_uro_filtered_100_${strand}
+pcov_file=${outdir}u10_uro_filtered_100_peak_count_updated.rds
+fprefix=u10_uro_filtered_100
 w=11
 h=8
 x=100
@@ -63,7 +62,7 @@ echo filter peak ref
 
 min_cov=10
 out='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3c_updated_peaks/'
-fprefix=u10_uro_filtered_100_${strand}
+fprefix=u10_uro_filtered_100
 
 Rscript ${script_dir}3b_filter_peak_ref.R ${ref} ${pcov_file} ${out} ${fprefix} ${min_cov} ${is_minus}
 
