895d2654bd576081647ae0ccdce4816714a1f088
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-14 02:28:52 -0500

    Update 3b_update_peak_ref.sh

--- Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
+++ Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
@@ -2,40 +2,39 @@
 
 #SBATCH --mail-type=END
 #SBATCH --mail-user=sonas@ccf.org
-#SBATCH --job-name=final_peak_ref
-#SBATCH -n 20 -p xtreme
+#SBATCH --job-name=peak_cov
+#SBATCH -n 30
 #SBATCH --mem-per-cpu=3900
 
-#SBATCH -o peak_cov%J.out
-#SBATCH -e peak_cov%J.err
-
-
-### Load modules ###
+#SBATCH -o peak_cov_%J.out
+#SBATCH -e peak_cov_%J.err
 
 module load R/4.1.0
 
 ### Initialize variables ###
 
-strand='minus'
-is_minus='TRUE'
+#strand='minus'
+#is_minus='TRUE'
 
-#strand='plus'
-#is_minus='FALSE'
+strand='plus'
+is_minus='FALSE'
 
 cores=30
 bam_dir='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1e_merged_bam/'
-bam=${bam_dir}dedup_u10_uro_clean_filtered_${strand}.bam
+#bam=${bam_dir}dedup_u10_uro_clean_filtered_${strand}.bam
+bam=${bam_dir}dedup_u10_uro_clean_filtered2_2_${strand}.bam
 
 ref_dir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3a_assign_TU/'
 ref_saf=${ref_dir}u10_uro_filtered_100_peak_universe_updated.saf
 
 outdir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage/'
-fprefix=u10_uro_${strand}
+fprefix=u10_uro_filtered_100_${strand}
 
 script_dir='/home/sonas/beegfs/APA/scPASU/scripts/'
 
 # 1. Calculate peak counts
 
+echo peak counts
 Rscript ${script_dir}feature_counts.R ${bam} ${ref_saf} ${outdir} ${fprefix} ${cores} no
 
 # 2. Calculate per TU peak coverage
@@ -43,23 +42,28 @@ Rscript ${script_dir}feature_counts.R ${bam} ${ref_saf} ${outdir} ${fprefix} ${c
 counts_file=${outdir}${fprefix}_peak_count.rds
 ref=${ref_dir}u10_uro_filtered_100_peak_universe_updated.txt
 
+echo get peak coverage
 Rscript ${script_dir}3b_peaks_coverage.R ${ref} ${counts_file} ${outdir}
+
 
 # 3.  Plot peak coverage percent
 
-pcov_file=${outdir}u10_uro_${strand}_peak_count_updated.rds
-fprefix=u10_uro_${strand}
-#w=11
-#h=8
-#x=100
-#y=5000
+echo peak coverage plot 
+pcov_file=${outdir}u10_uro_filtered_100_${strand}_peak_count_updated.rds
+fprefix=u10_uro_filtered_100_${strand}
+w=11
+h=8
+x=100
+y=5000
 
-#Rscript ${script_dir}3b_plot_peak_cov.R ${pcov_file} ${outdir} ${fprefix} ${w} ${h} ${x} ${y}
+Rscript ${script_dir}3b_plot_peak_cov.R ${pcov_file} ${outdir} ${fprefix} ${w} ${h} ${x} ${y}
 
 # 4. Filter peak count mat
+echo filter peak ref
+
 min_cov=10
 out='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3c_updated_peaks/'
-fprefix=u10_uro_${strand}_filtered_
+fprefix=u10_uro_filtered_100_${strand}
 
 Rscript ${script_dir}3b_filter_peak_ref.R ${ref} ${pcov_file} ${out} ${fprefix} ${min_cov} ${is_minus}
 
