65a1ea10fdccc430065e470a990ea7a2ea347e0b
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-15 13:43:27 -0500

    Update 3b_update_peak_ref.sh

--- Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
+++ Scripts/3_peak_ref_cleanup/3b_update_peak_ref.sh
@@ -13,52 +13,72 @@ module load R/4.1.0
 
 ### Initialize variables ###
 
-#strand='minus'
-#is_minus='TRUE'
-
-#strand='plus'
-#is_minus='FALSE'
-
 cores=30
+script_dir='/home/sonas/beegfs/APA/scPASU/scripts/'
+fprefix='u10_uro'
+
+ref_dir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3a_assign_TU/'
+ref=${ref_dir}${fprefix}_peak_universe_updated.txt
+
+outdir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/'
+
+mkdir -p ${outdir}
+
+# 1a. Filter by PR length
+
+echo filter by PR width
+
+maxwidth=1000
+out1=${outdir}3b_Filter_by_PR_width/
+mkdir -p ${out1}
+
+Rscript ${script_dir}3b1_filter_by_pr_width.R ${ref} ${out1} ${fprefix} ${maxwidth}
+
+# 1b. Assign TU - to update peak ref
+
+echo  2nd TU assignment
+
+dir='/home/sonas/beegfs/APA/scPASU/'
+peak_ref_dir='none'
+ref2=${out1}${fprefix}_filtered_peak_ref.txt
+fprefix2=u10_uro_filtered_tu_assigned
+
+Rscript ${script_dir}3a_assign_tu.R 30 ${dir} ${peak_ref_dir} ${fprefix2} ${out1} ${gtf_file} ${ref2}
+
+# 2a. Calculate peak counts
+
+echo get peak counts
+
 bam_dir='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1e_merged_bam/'
 bam=${bam_dir}dedup_u10_uro_clean_filtered2_2_merged.bam
 
-fprefix=u10_uro
+ref_saf=${out1}${fprefix2}_peak_universe_updated.saf
+out2=${outdir}'3c_PeakCoverage/'
 
-ref_dir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3a_assign_TU/'
-ref_saf=${ref_dir}${fprefix}_peak_universe_updated.saf
+Rscript ${script_dir}feature_counts.R ${bam} ${ref_saf} ${out2} ${fprefix} ${cores} no
 
-outdir='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage/'
-
-script_dir='/home/sonas/beegfs/APA/scPASU/scripts/'
-
-# 1. Calculate peak counts
-
-echo peak counts
-Rscript ${script_dir}feature_counts.R ${bam} ${ref_saf} ${outdir} ${fprefix} ${cores} no
-
-# 2. Calculate per TU peak coverage
-
-counts_file=${outdir}${fprefix}_peak_count.rds
-ref=${ref_dir}u10_uro_filtered_100_peak_universe_updated.txt
+# 2b. Calculate per TU peak coverage
 
 echo get peak coverage
-Rscript ${script_dir}3b_peaks_coverage.R ${ref} ${counts_file} ${outdir}
 
+counts_file=${out2}${fprefix}_peak_count.rds
+ref3=${out1}${fprefix2}_peak_universe_updated.txt
 
-# 3.  Plot peak coverage percent
+Rscript ${script_dir}3b2_peaks_coverage.R ${ref3} ${counts_file} ${out2}
+
+# 2c.  Plot peak coverage percent
 
 echo peak coverage plot 
-pcov_file=${outdir}u10_uro_filtered_100_peak_count_updated.rds
+pcov_file=${out2}${fprefix}_peak_count_updated.rds
 fprefix=u10_uro_filtered_100
 w=11
 h=8
 x=100
 y=5000
 
-Rscript ${script_dir}3b_plot_peak_cov.R ${pcov_file} ${outdir} ${fprefix} ${w} ${h} ${x} ${y}
+Rscript ${script_dir}3b2_plot_peak_cov.R ${pcov_file} ${out2} ${fprefix} ${w} ${h} ${x} ${y}
 
-# 4. Filter peak count mat
+# 3. Filter peak count mat
 echo filter peak ref
 
 min_cov=10
