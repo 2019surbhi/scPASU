4e8f60234db5e9316704255f1049a2f92e86d865
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-15 14:46:17 -0500

    Create create_bed_file.R

---
+++ Scripts/7_ucsc_uploads/create_bed_file.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(data.table)
+
+args <- commandArgs(trailing = TRUE)
+
+
+ref_file=args[1]
+out=args[2]
+frefix=args[3]
+
+
+ref<-fread(ref_file)
+
+cols<-c('chr','start','end','final_annotation')
+bed<-ref[,..cols]
+bed$score<-rep(0,nrow(bed))
+bed$strand<-ref$strand
+
+write.table(bed, paste0(out,fprefix,'.bed'),sep='\t', quote=FALSE, col.names=FALSE, row.names=FALSE)
+
+
+
+
