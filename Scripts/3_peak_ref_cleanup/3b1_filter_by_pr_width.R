2c01d9a001183127f4cf08bdfbef3f4461941ffc
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-15 10:44:07 -0500

    Update 3b1_filter_by_pr_width.R

--- Scripts/3_peak_ref_cleanup/3b1_filter_by_pr_width.R
+++ Scripts/3_peak_ref_cleanup/3b1_filter_by_pr_width.R
@@ -7,7 +7,6 @@ args <- commandArgs(trailing = TRUE)
 
 
 ref_file=args[1]
-cov_mat_file=args[2]
 out=args[3]
 frefix=args[4]
 max_width=as.numeric(args[5])
@@ -15,12 +14,10 @@ max_width=as.numeric(args[5])
 # HPC paths
 
 #ref_file<-'/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3a_assign_TU/u10_uro_peak_universe_updated.txt'
-#cov_mat_file<-'/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3b_PeakCoverage2/u10_uro_minus_filtered_peak_count.rds'
 
 # Read inputs
 ref<-fread(ref_file)
-ref_fil<-cov_mat[which(ref$pr_width<=max_width),]
+ref_filtered<-ref[which(ref$pr_width<=max_width),]
 
 write.table(ref_filtered,paste0(out,frefix,'_filtered_peak_ref.txt'),col.names=TRUE,row.names=FALSE,sep='\t',quote=FALSE)
-
 
