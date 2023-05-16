534f382affcddc7e0f72610dda2cbd070903eb40
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-16 15:48:34 -0500

    Update 7_create_peak_ref_filtered_BAM.sh

--- Scripts/7_ucsc_uploads/7_create_peak_ref_filtered_BAM.sh
+++ Scripts/7_ucsc_uploads/7_create_peak_ref_filtered_BAM.sh
@@ -14,11 +14,11 @@
 
 # conda activate scPASU
 
-peak_ref='/home/sonas/beegfs/APA/scPASU/output/2_PeakRef/2e_TSS_filtered/u10_uro_l300iA10mm3_e100_updated_tss_filt_final.bed'
-BAM='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1d_merged_bam/u10_uro_merged.bam'
+peak_ref='/home/sonas/beegfs/APA/scPASU/output/3_RefinePeakRef/3d_final_ref/u10_uro_final.bed'
+BAM='/home/sonas/beegfs/APA/scPASU/output/1_process_bam/1e_merged_bam/dedup_u10_uro_clean_filtered2_2_merged.bam'
 out='/home/sonas/beegfs/APA/scPASU/output/7_UCSC_uploads/peak_filtered_bam/'
 
-bedtools intersect -abam ${BAM} -b ${peak_ref} -wa > ${out}u10_uro_peak_filtered.bam
+bedtools intersect -abam ${BAM} -b ${peak_ref} -s > ${out}u10_uro_peak_filtered.bam
 
 bam_dir='/home/sonas/beegfs/APA/scPASU/output/3_PeakCounts/3b_SplitBAM_per_clus/'
 bam_pre='2021_04_01_ureter10_uro_PC50_res0.2_cluster'
@@ -27,4 +27,4 @@ out='/home/sonas/beegfs/APA/scPASU/output/7_UCSC_uploads/peak_filtered_bam/'
 
 clus=$SLURM_ARRAY_TASK_ID
 
-bedtools intersect -abam ${bam_dir}${bam_pre}${clus}.bam -b ${peak_ref} -wa > ${out}cluster${clus}_peak_filtered.bam
+bedtools intersect -abam ${bam_dir}${bam_pre}${clus}.bam -b ${peak_ref} > ${out}cluster${clus}_peak_filtered.bam
