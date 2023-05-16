ea9e080f088ce4c4575cd057778b9ec7df2287b3
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-16 18:57:40 -0500

    Create 5a_merge_per_sample_counts.R

---
+++ Scripts/5_APA_testing/5a_merge_per_sample_counts.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+
+argv<-commandArgs(trailing = TRUE)
+
+count_dir=argv[1]
+fprefix=argv[2] 
+out=argv[3]
+
+count_dir<-'/home/sonas/APA/output/4_APA/inputs/counts_mat_dir/'
+
+f<-list.files(count_dir,full.names = TRUE)
+peak_counts<-lapply(f,read.table,sep='\t', header=TRUE,stringsAsFactors=FALSE)
+
+
+# Replace . with - in count mat 
+for(i in 1:length(peak_counts))
+{
+  colnames(peak_counts[[i]])<-gsub('.','-',colnames(peak_counts[[i]]),fixed = TRUE)
+  colnames(peak_counts[[i]])<-gsub('uro_','',colnames(peak_counts[[i]]))
+}
+
+# Merge counts
+merged_counts<-do.call(cbind,peak_counts)
+
+write.table(merged_counts,paste0(out,fprefix,'_counts.txt'),sep='\t')
