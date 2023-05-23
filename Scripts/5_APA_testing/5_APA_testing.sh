ddd3746f2571df8ec41332b1b19eb9bb40a13d34
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-05-23 10:03:45 -0500

    Update 5_APA_testing.sh

--- Scripts/5_APA_testing/5_APA_testing.sh
+++ Scripts/5_APA_testing/5_APA_testing.sh
@@ -19,7 +19,7 @@ script_dir='/home/sonas/beegfs/APA/scPASU/scripts/'
 ### 1. Merge per sample counts to a single matrix ###
 
 counts_dir='/home/sonas/beegfs/APA/scPASU/output/4_PeakCounts/4d_merged_PeakMat/'
-fprefix='u101_uro'
+fprefix='u10_uro'
 out='/home/sonas/beegfs/APA/scPASU/output/5_APA_testing/inputs/'
 
 Rscript ${script_dir}5a_merge_per_sample_counts.R ${counts_dir} ${fprefix} ${out}
